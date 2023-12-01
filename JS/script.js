e4dfd8ba51beb587cce0236f68556468370dda36
EmirB08 <142892269+EmirB08@users.noreply.github.com>
2023-12-01 14:11:54 +0100

    just for test

--- JS/script.js
+++ JS/script.js
@@ -15,16 +15,11 @@ const createContainer = (containerId) => {
     return container;
 };
 
-
-
-
-
 // Function to create a new button element with specified text and an event handler. - can be used to create additional elements with specified text and event handler
 const createQuizButton = (buttonText, eventHandler) => {
     const button = document.createElement("button");
     button.textContent = buttonText;
     button.addEventListener("click", eventHandler);
-    button.classList.add("quiz-button"); 
     return button;
 };
 
@@ -52,7 +47,6 @@ const setupQuizContainers = () => {
     quizQuestionContainer.id = "quiz-question-container";
     document.body.appendChild(quizCategoryContainer);
     document.body.appendChild(quizQuestionContainer);
-    
 };
 
 // Function to initialize and display quiz categories.
@@ -65,7 +59,6 @@ const initializeQuizCategories = () => {
         quizCategoryContainer.appendChild(createQuizButton(quizCategory.categoryName, () => selectQuizCategory(quizCategory.categoryName)));
     });
 };
-let quizStartTime; // Variable to store the start time of the quiz
 
 // Function to handle selection of a quiz category.
 const selectQuizCategory = (selectedCategoryName) => {
@@ -74,9 +67,6 @@ const selectQuizCategory = (selectedCategoryName) => {
         console.error("Category not found:", selectedCategoryName);
         return;
     }
- // Record the start time when the quiz category is selected
-  quizStartTime = new Date();
-
 
     currentQuizCategory = selectedCategory;
     currentQuizQuestionIndex = 0;
@@ -86,14 +76,6 @@ const selectQuizCategory = (selectedCategoryName) => {
     displayQuizQuestion(); // Display the first question of the selected category
 };
 
-// Function to display the quiz progress (current question number and total number of questions).(ilakia)
-const displayQuizProgress = () => {
-    const progressText = `Question ${currentQuizQuestionIndex + 1} of ${currentQuizCategory.questionArray.length}`;
-    const progressElement = createElementWithText("p", progressText);
-    quizQuestionContainer.appendChild(progressElement);
-};
-
-
 
 // Function to display the current quiz question and answer options.
 const displayQuizQuestion = () => {
@@ -101,9 +83,6 @@ const displayQuizQuestion = () => {
 
     const categoryTitle = createElementWithText("h2", currentQuizCategory.categoryName);
     quizQuestionContainer.appendChild(categoryTitle);
-
-    // Display quiz progress
-    displayQuizProgress();
 
     const currentQuestion = currentQuizCategory.questionArray[currentQuizQuestionIndex];
     quizQuestionContainer.appendChild(createElementWithText("h2", currentQuestion.questionText));
@@ -115,20 +94,15 @@ const displayQuizQuestion = () => {
         quizQuestionContainer.appendChild(answerButton);
     });
 
-   // Append 'Previous' and 'Next' or 'Finish Quiz' buttons based on the current question index.
-   if (currentQuizQuestionIndex > 0) {
-    quizQuestionContainer.appendChild(createQuizButton("Previous", previousQuizQuestion));
-   }
-   if (currentQuizQuestionIndex < currentQuizCategory.questionArray.length - 1) {
+    // Append 'Previous' and 'Next' or 'Finish Quiz' buttons based on the current question index.
+    if (currentQuizQuestionIndex > 0) {
+        quizQuestionContainer.appendChild(createQuizButton("Previous", previousQuizQuestion));
+    }
+    if (currentQuizQuestionIndex < currentQuizCategory.questionArray.length - 1) {
         quizQuestionContainer.appendChild(createQuizButton("Next", nextQuizQuestion));
-
-       
-   } else {
+    } else {
         quizQuestionContainer.appendChild(createQuizButton("Finish Quiz", showQuizEndPage));
-        finishQuizButton.id = "review-button"; // Set the ID for the review button
-        finishQuizButton.disabled = true; // Disable the review button initially
-        quizQuestionContainer.appendChild(finishQuizButton);
-   }
+    }
 };
 
 // Function to handle the selection of an answer.
@@ -137,19 +111,7 @@ const handleAnswerSelection = (selectedButton, isCorrect) => {
     answerButtons.forEach(button => button.classList.remove("selected-answer"));
     selectedButton.classList.add("selected-answer");
     if (isCorrect) userQuizScore++;
-    
-    // Enable the review button after an answer is selected(ilakia)
-    enableReviewButton();
 };
-
-/* 
-// Function to enable the review button.(ilakia)
-const enableReviewButton = () => {
-    const reviewButton = document.getElementById("review-button");
-    if (reviewButton) {
-        reviewButton.disabled = false;
-    }
-}; */
 
 // Functions to navigate to the previous and next quiz questions.
 const previousQuizQuestion = () => {
@@ -162,39 +124,13 @@ const nextQuizQuestion = () => {
     displayQuizQuestion();
 };
 
-
+// Function to display the end page of the quiz showing the user's score.
 const showQuizEndPage = () => {
     clearContainerChildren(quizQuestionContainer);
     quizQuestionContainer.appendChild(createElementWithText("h2", "Quiz Completed!"));
     quizQuestionContainer.appendChild(createElementWithText("p", `Your score: ${userQuizScore}/${currentQuizCategory.questionArray.length}`));
-
-    // Calculate the time taken(ilakia)
-    const quizEndTime = new Date();
-    const timeTaken = (quizEndTime - quizStartTime) / 1000; // Convert milliseconds to seconds
-
-    // Display the time taken
-    quizQuestionContainer.appendChild(createElementWithText("p", `Time taken: ${timeTaken} seconds`));
-
+    quizQuestionContainer.appendChild(createQuizButton("Restart Quiz", resetQuiz));
 };
-
-
-// Function to finish the quiz and return to the category selection or home.
-const finishQuiz = (autoFinish) => {
-    // Clear the container
-    clearContainerChildren(quizQuestionContainer);
-
-    if (autoFinish) {
-        // Automatically finish the quiz after displaying the results
-        setTimeout(() => {
-            resetQuiz();
-        }, 10000); //
-    } else {
-        // Keep the results displayed until the user clicks home
-        quizCategoryContainer.style.display = "";
-        initializeQuizCategories();
-    }
-};
-
 
 // Function to reset the quiz and return to the category selection.
 const resetQuiz = () => {
@@ -206,63 +142,6 @@ const resetQuiz = () => {
     initializeQuizCategories();
 };
 
-
-// Function to create a home button with an event handler.(ilakia)
-const createHomeButton = (eventHandler) => {
-    const homeButton = document.createElement("button");
-    homeButton.textContent = "Home";
-    homeButton.addEventListener("click", eventHandler);
-    return homeButton;
-};
-
-
-
 // Initialize the quiz containers and display categories.
 setupQuizContainers();
 initializeQuizCategories();
-
-
-// Create and append the home button.(ilakia)
-const homeButton = createHomeButton(resetQuiz); 
-document.body.appendChild(homeButton);
-
-
-
-// Set the style for the home button
-homeButton.style.position = "absolute";
-homeButton.style.top = "10px"; 
-homeButton.style.right = "10px"; 
-/* 
-// Function to review the selected answers (ilakia)
-const reviewAnswers = () => {
-    clearContainerChildren(quizQuestionContainer);
-
-    // Display each question with user's selected answer and correct answer
-    currentQuizCategory.questionArray.forEach((question, index) => {
-        const questionContainer = createContainer(`question-${index}`);
-        questionContainer.appendChild(createElementWithText("h3", question.questionText));
-
-        // Display user's selected answer
-        const userAnswer = createElementWithText("p", `Your Answer: ${question.answers.find(answer => answer.isSelected)?.answerText || "Not answered"}`);
-        questionContainer.appendChild(userAnswer);
-
-        // Display correct answer
-        const correctAnswer = createElementWithText("p", `Correct Answer: ${question.answers.find(answer => answer.isCorrect)?.answerText}`);
-        questionContainer.appendChild(correctAnswer);
-
-        quizQuestionContainer.appendChild(questionContainer);
-    });
-
-    // Create and append 'Finish Quiz' button
-    quizQuestionContainer.appendChild(createQuizButton("Finish Quiz", () => finishQuiz(true)));
-};
-// Create the review button (added)
-const finishQuizButton = createQuizButton("Review Answers", reviewAnswers);
-finishQuizButton.id = "review-button"; // Set the ID for the review button
-finishQuizButton.disabled = true; // Disable the review button initially
-
- */
-
-
-
-
