9e724e75dd59c5084af8588ca33421dca1250ec3
EmirB08 <142892269+EmirB08@users.noreply.github.com>
2023-12-07 09:19:07 +0100

    added html quiz questions

--- JS/quizData.js
+++ JS/quizData.js
@@ -33,14 +33,138 @@ const quizObject = {
                         { answerText: "<section>", isCorrect: false },
                         { answerText: "<div>", isCorrect: false }
                     ]
+                },
+
+                {
+                    id: 4,
+                    questionText: "What is the purpose of the <title> tag in HTML?",
+                    answers: [
+                        { answerText: "To create the main title of the webpage", isCorrect: false },
+                        { answerText: "To set the title of the webpage as seen in browser tabs", isCorrect: true },
+                        { answerText: "To define the header of the webpage", isCorrect: false },
+                        { answerText: "To bold text in the webpage", isCorrect: false }
+                    ]
+                },
+                {
+                    id: 5,
+                    questionText: "Which tag is used to create a hyperlink in HTML?",
+                    answers: [
+                        { answerText: "<hyperlink>", isCorrect: false },
+                        { answerText: "<link>", isCorrect: false },
+                        { answerText: "<a>", isCorrect: true },
+                        { answerText: "<href>", isCorrect: false }
+                    ]
+                },
+                {
+                    id: 6,
+                    questionText: "What does the 'alt' attribute in an <img> tag specify?",
+                    answers: [
+                        { answerText: "Alternate text to be displayed if the image does not load", isCorrect: true },
+                        { answerText: "The height of the image", isCorrect: false },
+                        { answerText: "A link to the image source", isCorrect: false },
+                        { answerText: "The width of the image", isCorrect: false }
+                    ]
+                },
+                {
+                    id: 7,
+                    questionText: "Which HTML tag is used for creating an ordered list?",
+                    answers: [
+                        { answerText: "<ul>", isCorrect: false },
+                        { answerText: "<ol>", isCorrect: true },
+                        { answerText: "<li>", isCorrect: false },
+                        { answerText: "<list>", isCorrect: false }
+                    ]
+                },
+                {
+                    id: 8,
+                    questionText: "Which HTML element is used to define emphasized text?",
+                    answers: [
+                        { answerText: "<strong>", isCorrect: false },
+                        { answerText: "<em>", isCorrect: true },
+                        { answerText: "<mark>", isCorrect: false },
+                        { answerText: "<italic>", isCorrect: false }
+                    ]
+                },
+                {
+                    id: 9,
+                    questionText: "What is the correct HTML element for inserting a line break?",
+                    answers: [
+                        { answerText: "<break>", isCorrect: false },
+                        { answerText: "<lb>", isCorrect: false },
+                        { answerText: "<br>", isCorrect: true },
+                        { answerText: "<line>", isCorrect: false }
+                    ]
+                },
+                {
+                    id: 10,
+                    questionText: "Which HTML tag is used to define a table header?",
+                    answers: [
+                        { answerText: "<thead>", isCorrect: false },
+                        { answerText: "<th>", isCorrect: true },
+                        { answerText: "<header>", isCorrect: false },
+                        { answerText: "<top>", isCorrect: false }
+                    ]
+                },
+                {
+                    id: 11,
+                    questionText: "In HTML, what does the <canvas> element do?",
+                    answers: [
+                        { answerText: "Displays a 3D model", isCorrect: false },
+                        { answerText: "Used to draw graphics via scripting", isCorrect: true },
+                        { answerText: "Creates a container for images", isCorrect: false },
+                        { answerText: "Generates an animated canvas", isCorrect: false }
+                    ]
+                },
+                {
+                    id: 12,
+                    questionText: "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?",
+                    answers: [
+                        { answerText: "src", isCorrect: false },
+                        { answerText: "title", isCorrect: false },
+                        { answerText: "alt", isCorrect: true },
+                        { answerText: "href", isCorrect: false }
+                    ]
+                },
+                {
+                    id: 13,
+                    questionText: "Which HTML element is used to specify a header for a document or section?",
+                    answers: [
+                        { answerText: "<header>", isCorrect: true },
+                        { answerText: "<top>", isCorrect: false },
+                        { answerText: "<head>", isCorrect: false },
+                        { answerText: "<section>", isCorrect: false }
+                    ]
+                },
+                {
+                    id: 14,
+                    questionText: "What is the function of the <iframe> tag in HTML?",
+                    answers: [
+                        { answerText: "To create an interactive frame within a webpage", isCorrect: false },
+                        { answerText: "To display a nested webpage", isCorrect: true },
+                        { answerText: "To insert an image into the webpage", isCorrect: false },
+                        { answerText: "To frame a section of text", isCorrect: false }
+                    ]
+                },
+                {
+                    id: 15,
+                    questionText: "What is the correct HTML for creating a checkbox?",
+                    answers: [
+                        { answerText: "<input type='box'>", isCorrect: false },
+                        { answerText: "<check>", isCorrect: false },
+                        { answerText: "<input type='checkbox'>", isCorrect: true },
+                        { answerText: "<box>", isCorrect: false }
+                    ]
                 }
-            ]
+                
+            ]   
+
         },
+
         {
             categoryName: "CSS",
             questionArray: [
                 {   
-                    id: 4,
+                    id: 16,
                     questionText: "What does CSS stand for?",
                     answers: [
                         { answerText: "Colorful Style Sheets", isCorrect: false },
@@ -50,7 +174,7 @@ const quizObject = {
                     ]
                 },
                 {   
-                    id: 5,
+                    id: 17,
                     questionText: "Which property is used to change the font of an element?",
                     answers: [
                         { answerText: "font-type", isCorrect: false },
@@ -60,7 +184,7 @@ const quizObject = {
                     ]
                 },
                 {   
-                    id: 6,
+                    id: 18,
                     questionText: "How do you select an element with the id 'demo'?",
                     answers: [
                         { answerText: ".demo", isCorrect: false },
@@ -71,10 +195,11 @@ const quizObject = {
                 }
             ]
         },
+
         {
             categoryName: "JavaScript",
             questionArray: [
-                {   id: 7,
+                {   id: 19,
                     questionText: "Which symbol is used for comments in JavaScript?",
                     answers: [
                         { answerText: "//", isCorrect: true },
@@ -84,7 +209,7 @@ const quizObject = {
                     ]
                 },
                 {   
-                    id: 8,
+                    id: 20,
                     questionText: "Which method is used to write messages in the console?",
                     answers: [
                         { answerText: "console.write()", isCorrect: false },
@@ -94,7 +219,7 @@ const quizObject = {
                     ]
                 },
                 {   
-                    id: 9,
+                    id: 21,
                     questionText: "Which of the following is a JavaScript data type?",
                     answers: [
                         { answerText: "Number", isCorrect: true },
